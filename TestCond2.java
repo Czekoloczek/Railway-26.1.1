22feaa0d6e08abb007908641e5bf3ba69384b6a5
Poppy <yihanli163@gmail.com>
2026-04-03 15:59:52 -0700

    Delete TestCond2.java

--- TestCond2.java
+++
@@ -1,6 +0,0 @@
-import net.neoforged.neoforge.common.conditions.ModLoadedCondition;
-public class TestCond2 {
-    public static void main(String[] args) {
-        System.out.println(ModLoadedCondition.class.getName());
-    }
-}
